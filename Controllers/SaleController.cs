a274968409eae4fce13816c65a6da3f400ed4ef0
hampadco <tak1.ghasemi@gmail.com>
2023-05-19 01:07:00 +0330

    first commit

---
+++ Controllers/SaleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Dammon.Models;
+
+namespace Dammon.Controllers;
+
+public class SaleController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public SaleController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
