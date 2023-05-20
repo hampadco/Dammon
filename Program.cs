3a51a79b5254e0f2de12cb280c20cfecd9cbd1a9
hampadco <tak1.ghasemi@gmail.com>
2023-05-20 11:25:44 +0330

    30ordibehesht

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+           if (app.Environment.IsDevelopment())
+            {
+                 //app.UseDeveloperExceptionPage();
+                 app.UseExceptionHandler("/Home/offline");
+                
+            }
+            else
+            {
+                // app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
+                  app.UseExceptionHandler("/Home/offline");
+            }
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -22,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=login}/{action=Index}/{id?}");
 
 app.Run();
